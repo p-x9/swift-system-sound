d6d9a8d900be6507ca0d1ebbee4badc6a09824f1
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-03-18 09:08:19 +0900

    Implemented `SystemSoundKind`

---
+++ Sources/SystemSound/SystemSoundKind.swift
@@ -0,0 +1,27 @@
+//
+//  SystemSoundKind.swift
+//  SystemSound
+//
+//  Created by p-x9 on 2025/03/18
+//  
+//
+
+import Foundation
+
+public enum SystemSoundKind {
+    case `default`
+    case modern
+    case new
+    case nano
+}
+
+extension SystemSoundKind {
+    public var directoryName: String {
+        switch self {
+        case .default: "."
+        case .modern: "Modern"
+        case .new: "New"
+        case .nano: "nano"
+        }
+    }
+}
